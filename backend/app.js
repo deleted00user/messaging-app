bf2d59a790d14281e99e620e643c5bb026cdbf80
Nikita <gs272w@gmail.com>
2025-11-09 14:05:46 +0100

    Added the auth middleware

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,20 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const authenticateToken = require('./middleware/authMiddleware');
 
 app.use(express.json())
 app.use(cors());
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
+
+app.get('/api/test-auth', authenticateToken, (req, res) => {
+    res.json({ 
+        message: 'You are authenticated!', 
+        userId: req.user.userId 
+    });
+});
 
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`)
